08550c7e446f196f61004022b6fb6f9320436980
Fxx Morgan <FxxMorgan@outlook.cl>
2022-10-29 20:06:15 -0300

    Style + Chapters

---
+++ js/boton.js
@@ -0,0 +1,22 @@
+//Todos los elementos a los que les vamos a cambiar el fontSize
+const elementsList = document.getElementsByTagName('p','li');
+
+function getElementFontSize(element){
+  //getComputedStyle nos devuelve las propiedades css de cada párrafo(elemento)
+  const elementFontSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+  return parseFloat(elementFontSize);  //Devolvemos el total de pixeles
+}
+
+function cambiarTexto(operador) {
+   for(let element of elementsList) {
+     //Obtener el total de pixel de cada párrafo.
+     const currentSize = getElementFontSize(element);
+     
+     //Restar o sumar, dependiendo del operador.
+     const newFontSize = (operador === '+' ? (currentSize + 1) : (currentSize - 1)) + 'px';
+     //Aplicarle al parrafo actual el nuevo tamaño.
+     element.style.fontSize = newFontSize
+   }
+}
+
+
