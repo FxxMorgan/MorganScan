af4652b51309a1ef826591c55bff7aa06bd1f252
Fxx Morgan <Tu direccin de correo electrnico aqu>
2023-04-15 22:37:15 -0400

    404 add

--- js/boton.js
+++ js/boton.js
@@ -1,23 +1,16 @@
-//Todos los elementos a los que les vamos a cambiar el fontSize
-const elementsList = document.getElementsByTagName('p','li');
 
-function getElementFontSize(element){
-  //getComputedStyle nos devuelve las propiedades css de cada párrafo(elemento)
-  const elementFontSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
-  return parseFloat(elementFontSize);  //Devolvemos el total de pixeles
+const paragraphElements = document.querySelectorAll('p, li');
+
+function getFontSizeInPixels(element) {
+  const fontSize = window.getComputedStyle(element, null).getPropertyValue('font-size');
+  return parseFloat(fontSize);
 }
 
-function cambiarTexto(operador) {
-   for(let element of elementsList) {
-     //Obtener el total de pixel de cada párrafo.
-     const currentSize = getElementFontSize(element);
-     
-     //Restar o sumar, dependiendo del operador.
-     const newFontSize = (operador === '+' ? (currentSize + 1) : (currentSize - 1)) + 'px';
-     //Aplicarle al parrafo actual el nuevo tamaño.
-     element.style.fontSize = newFontSize
-   }
+function cambiarTexto(operator) {
+  const sizesInPixels = Array.from(paragraphElements, getFontSizeInPixels);
+  const newSizesInPixels = sizesInPixels.map(size => operator === '+' ? size + 1 : size - 1);
+  
+  paragraphElements.forEach((element, index) => {
+    element.style.fontSize = `${newSizesInPixels[index]}px`;
+  });
 }
-
-
-
